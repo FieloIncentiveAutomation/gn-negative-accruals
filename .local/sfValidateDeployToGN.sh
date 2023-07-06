811931483ff5d34a9fdfc65f1593e102ec9709f4
Tiago Leal <tibeal@gmail.com>
2023-07-06 14:01:09 -0300

    new stuff

--- .local/sfValidateDeployToGN.sh
+++ .local/sfValidateDeployToGN.sh
@@ -3,7 +3,7 @@ echo "Setting instance" &&
 sf config set org-instance-url=https://CS81.salesforce.com;
 
 echo "Setting session Id" &&
-session_id='00D260000009Ikd!ARIAQHPkRIOtVcuooE9Yk4VxtqPZu0RLNCRI89cke89zZmc7VVoQVwkiZOwNm5o.fXSvnDr5AFzRTWoCpFt_4uhixKqW_D0l';
+session_id='00D260000009Ikd!ARIAQMCiAuiHPxv4zX9U7RB.cxkmiuiQNwAOL0MH64GkNxdqJtau_4XPuLFL0ggNtbLqFFhB7syYy79sftTgp7po9NukSpIN';
 
 sf project deploy start \
     --metadata ApexClass \
